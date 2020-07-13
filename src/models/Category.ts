a33a03cfd37794408ebd6bf9197fd31f12b81a51
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-07-13 06:49:27 -0400

    Finish migrations and models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('category')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
